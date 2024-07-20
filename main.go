6a98027b8e55cb4764550a8a8c3c191c85d2aabd
facuellarg <facuellarg@unal.edu.co>
2024-07-20 16:45:20 -0500

    add main

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import "os"
+
 func main() {
+	rows, err := ReadRenpyInfo("test.xlsx", "start")
+	if err != nil {
+		panic(err)
+	}
+
+	writer := NewWriter("./templates/excel_to_renpy.tmpl")
+	text, err := writer.RenpyInfoToText(rows)
+	if err != nil {
+		panic(err)
+	}
+	print(text)
+	//save text in renpy file
+	if err := os.WriteFile("renpy.rpy", []byte(text), 0644); err != nil {
+		panic(err)
+	}
 
 }
---
+++ renpy.rpy
@@ -0,0 +1,9 @@
+define John = Character("John")
+
+label start:
+   John "Hello"
+   John "How are you?"
+
+   scene imageScene
+
+
